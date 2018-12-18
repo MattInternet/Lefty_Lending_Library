1e59050cd60fe554560415ca8a013efa0ec8fc96
Matthew Monahan <matt17825@hotmail.com>
2018-12-18 14:58:39 -0700

    Made the UserMenu suck less

--- src/component/user/UserMenu.tsx
+++ src/component/user/UserMenu.tsx
@@ -1,17 +1,13 @@
 import * as React from 'react';
 
 import { observer, inject } from 'mobx-react';
-import { Typography, Button } from '@material-ui/core';
+import { Typography, Button, Menu, MenuItem, IconButton } from '@material-ui/core';
 import { withStyles } from '@material-ui/core';
 
 export interface IUserMenuProps {
     authStore?: any;
     className?: string;
     classes?: any;
-}
-
-interface IUserMenuState {
-    menuOpen: boolean;
 }
 
 const styles: any = (theme: any) => ({
@@ -22,14 +18,15 @@ const styles: any = (theme: any) => ({
 
 @inject('authStore')
 @observer
-class UserMenu extends React.Component<IUserMenuProps, IUserMenuState> {
+class UserMenu extends React.Component<IUserMenuProps, any> {
     state = {
-        menuOpen: false,
+        anchorEl: null
     };
 
     public render() {
         const { authStore, className, classes } = this.props;
-        const { isLoggedIn, login, initializing, logout } = authStore;
+        const { isLoggedIn, login, initializing} = authStore;
+        const { anchorEl } = this.state;
 
         if (initializing) {
             return <Typography>INITIALIZING</Typography>;
@@ -42,26 +39,37 @@ class UserMenu extends React.Component<IUserMenuProps, IUserMenuState> {
                 </Button>
             )
         }
-        return(
-            <Button onClick={logout} className={className || classes.userMenu} color="inherit">
-                    Sign Out
-            </Button>
+        return (
+            <div className={classes.userMenu}>
+                <IconButton aria-owns={anchorEl ? 'simple-menu' : undefined}
+                aria-haspopup="true"
+                onClick={this.handleOpen}>🌹</IconButton>
+                
+                <Menu
+                    id="simple-menu"
+                    anchorEl={anchorEl}
+                    open={Boolean(anchorEl)}
+                    onClose={this.handleClose}>
+                    <MenuItem onClick={this.handleClose}>My account</MenuItem>
+                    <MenuItem onClick={this.handleLogout}>Logout</MenuItem>
+                </Menu>
+            </div>
         )
-        
+
     }
 
-    // private handleLogout = () => {
-    //     this.props.authStore.logout();
-    //     this.handleMenuClose();
-    // }
+    handleOpen= (event:any) => {
+        this.setState({ anchorEl: event.currentTarget });
+    };
 
-    // private handleMenuOpen = (event: any) => {
-    //     this.setState({ menuOpen: true });
-    // }
+    handleClose = () => {
+        this.setState({ anchorEl: null });
+    };
 
-    // private handleMenuClose = () => {
-    //     this.setState({ menuOpen: false });
-    // }
+    private handleLogout = () => {
+        this.props.authStore.logout();
+        this.handleClose();
+    }
 }
 
 export default withStyles(styles)(UserMenu);
