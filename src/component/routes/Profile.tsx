db8e1fd8c93f81020a9afdb167f460ba29f366f9
Matthew Monahan <matt17825@hotmail.com>
2018-12-18 17:26:11 -0700

    Added a Floating Action Button (FAB) to the Profile page

--- src/component/routes/Profile.tsx
+++ src/component/routes/Profile.tsx
@@ -1,13 +1,28 @@
 import * as React from 'react';
+import { Fab, withStyles } from '@material-ui/core';
+import AddIcon from '@material-ui/icons/Add'
+
+const styles:any = theme => ({
+    fab: {
+        position: 'absolute',
+        bottom: theme.spacing.unit * 2,
+        right: theme.spacing.unit * 2
+    }
+});
 
 class Profile extends React.Component<any, any> {
     public render() {
+        const { classes } = this.props;
+
         return(
             <div>
                 PROFILE!!!!! 😎
+                <Fab className={classes.fab}>
+                    <AddIcon/>
+                </Fab>
             </div>
         )
     }
 }
 
-export default Profile;
+export default withStyles(styles)(Profile);
